dc6ae0246d751e0b9e6fafd3fab0307013c0c3ad
Mali-r <ichmali.24@gmail.com>
2026-09-08 11:55:48 +0700

    Add usersSupabaseRoutes to router

--- backend/src/routes/v2/index.js
+++ backend/src/routes/v2/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { router as userRoutes } from "./users.routes.js";
+import { router as usersRoutes } from "./users.routes.js";
+import { router as usersSupabaseRoutes } from "./users.supabase.routes.js";
 
 export const router = Router();
 
-router.use("/users",userRoutes);
+router.use("/users",usersRoutes);
+router.use("/users",usersSupabaseRoutes);
